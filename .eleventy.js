2c4eee0fc5659a0b4d59e9b7502d24ea3f92e92d
Jorge Monteiro Jr <junior@w3is.net>
2023-04-08 19:54:12 -0300

    Mais um teste com baseUrl

--- .eleventy.js
+++ .eleventy.js
@@ -2,16 +2,23 @@ const { EleventyHtmlBasePlugin } = require("@11ty/eleventy");
 
 module.exports = function(eleventyConfig) {
 
-  eleventyConfig.addPassthroughCopy('src/css')
+  eleventyConfig.addPassthroughCopy('src/css');
+
+  eleventyConfig.addPlugin(EleventyHtmlBasePlugin, {
+    baseHref: eleventyConfig.pathPrefix,
+    extensions: "html",
+    filters: {
+      base: "htmlBaseUrl",
+      html: "transformWithHtmlBase",
+      pathPrefix: "addPathPrefixToUrl",
+    },
+  });
   
   return {
     dir: {
       input: "src",
       output: "docs"
     },
-    passthroughFileCopy: true,
-    pathPrefix: "/nt-interlinear"
+    passthroughFileCopy: true
   }
-
-  eleventyConfig.addPlugin(EleventyHtmlBasePlugin)
 }
