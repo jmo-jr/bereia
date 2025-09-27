a7cc435ba7e208258d671465b3be7f7177131c66
Jorge Monteiro Jr <junior@w3is.net>
2025-09-27 11:26:05 -0300

    Mudei estrutura de pastas

--- .eleventy.js
+++ .eleventy.js
@@ -1,3 +1,4 @@
+const { execSync } = require('child_process')
 //const { EleventyHtmlBasePlugin } = require("@11ty/eleventy");
 
 module.exports = function(eleventyConfig) {
@@ -7,6 +8,11 @@ module.exports = function(eleventyConfig) {
 
   // Habilita o plugin que adiciona utilitários para URLs com pathPrefix
   //eleventyConfig.addPlugin(EleventyHtmlBasePlugin);
+
+	//PageFind search
+	eleventyConfig.on('eleventy.after', () => {
+    execSync(`npx pagefind --site _site/biblia --output-subdir pagefind --glob \"**/*.html\"`, { encoding: 'utf-8' })
+  })
 
   return {
     // ajuste para o prefixo do site hospedado (terminar com "/")
