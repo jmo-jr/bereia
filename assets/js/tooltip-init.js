b81ba2d077aaa8c8d72c10ce185e8ee87f9060f0
jmo-jr <jmo-jr@users.noreply.github.com>
2025-12-05 14:44:49 +0000

    deploy: d1511fffefcaeba46a9bbe5d3869b08f5272c1df

--- assets/js/tooltip-init.js
+++ assets/js/tooltip-init.js
@@ -3,6 +3,15 @@ $(document).ready(function () {
     theme: 'tooltipster-light',
     animation: 'fade',
     delay: 100,
-		maxWidth: 400
+		maxWidth: 400,
+		trigger: 'custom',
+    triggerOpen: {
+        click: true,
+        tap: true
+    },
+    triggerClose: {
+        click: true,
+        tap: true
+    }
   });
 });
