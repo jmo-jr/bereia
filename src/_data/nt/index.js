d4d20ac2c6adcb54cfeac4ebe0b736fd1ea2f9c2
Jorge Monteiro Jr <junior@w3is.net>
2026-08-15 15:58:30 -0300

    mt 13a

--- src/_data/nt/index.js
+++ src/_data/nt/index.js
@@ -7,7 +7,7 @@ const SOURCE_DIR = path.join(__dirname, '..', '..', 'interlinear', 'nt');
 const DICT_FIELDS = ['strongs', 'grego', 'transliteracao', 'verbete', 'ocorrencias', 'traducao', 'pt', 'morfologia', 'abrev_morf' ];
 
 // Terms that should keep diacritics to avoid collapsing homographs.
-const NORMALIZATION_EXCEPTIONS = new Set(["α", "εν", "η", "ης", "ην", "ητε", "ου", "ει", "ως", "ο", "ος", "αν", "τις", "που", "πως", "αυτου", "αυτη", "δη", "ανω", "ημερα", "εκτος", "τι", "εις", "τινι", "γενεας", "ετερα", "τινες"]);
+const NORMALIZATION_EXCEPTIONS = new Set(["α", "εν", "η", "ης", "ην", "ητε", "ου", "ει", "ως", "ο", "ος", "αν", "τις", "που", "πως", "αυτου", "αυτη", "δη", "ανω", "ημερα", "εκτος", "τι", "εις", "τινι", "γενεας", "ετερα", "τινες", "ηλιου"]);
 
 const loadJsonFile = filePath => JSON.parse(fs.readFileSync(filePath, 'utf8'));
 
