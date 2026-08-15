4b8f42310425aa2c4b27d9461ad53f2c9934bd2c
Jorge Monteiro Jr <junior@w3is.net>
2026-08-15 17:05:59 -0300

    pwa

--- .eleventy.js
+++ .eleventy.js
@@ -6,6 +6,8 @@ module.exports = function(eleventyConfig) {
   eleventyConfig.addPassthroughCopy('src/css');
   eleventyConfig.addPassthroughCopy('src/img');
 	eleventyConfig.addPassthroughCopy("src/assets");
+	eleventyConfig.addPassthroughCopy("src/manifest.json");
+	eleventyConfig.addPassthroughCopy("src/assets/icons");
 
 	//PageFind search
 	eleventyConfig.on('eleventy.after', () => {
