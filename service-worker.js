42d55ffef611d0473d5326de1f2362aab093d40f
jmo-jr <jmo-jr@users.noreply.github.com>
2026-08-15 20:25:00 +0000

    deploy: f7c14fe68a2a9c6fb840d4824ae972072d7a9cfa

---
+++ service-worker.js
@@ -0,0 +1,14 @@
+const CACHE = "bereia-v1";
+self.addEventListener("install", evt => {
+  evt.waitUntil(
+    caches.open(CACHE).then(cache => cache.addAll([
+      "/bereia/",
+      "/bereia/css/style.css",
+      "/bereia/assets/js/tooltipster.bundle.min.js"
+    ]))
+  );
+  self.skipWaiting();
+});
+self.addEventListener("fetch", evt => {
+  evt.respondWith(caches.match(evt.request).then(r => r || fetch(evt.request)));
+});
